6683d94f9cbc5a4c0e60f9d7aa56fcd256d8b14a
Trey Hall <hallta3199@gmail.com>
2024-04-13 22:46:02 -0400

    Began work on a card filter control to filter through all known cards

---
+++ mtg-library/mtg-library/Controls/CardFilterView.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace mtg_library.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CardFilterView : ContentView
+    {
+        public string CardText { get; set; }
+
+        public bool FilterBlackMana { get; set; } = false;
+        public bool FilterBlueMana { get; set; } = false;
+        public bool FilterRedMana { get; set; } = false;
+        public bool FilterGreenMana { get; set; } = false;
+        public bool FilterWhiteMana { get; set; } = false;
+
+
+        public CardFilterView()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFilterBlackMana_Clicked(object sender, EventArgs e)
+        {
+            FilterBlackMana = !FilterBlackMana;
+        }
+        private void btnFilterBlueMana_Clicked(object sender, EventArgs e)
+        {
+            FilterBlueMana = !FilterBlueMana;
+        }
+        private void btnFilterRedMana_Clicked(object sender, EventArgs e)
+        {
+            FilterRedMana = !FilterRedMana;
+        }
+        private void btnFilterGreenMana_Clicked(object sender, EventArgs e)
+        {
+            FilterGreenMana = !FilterGreenMana;
+        }
+        private void btnFilterWhiteMana_Clicked(object sender, EventArgs e)
+        {
+            FilterWhiteMana = !FilterWhiteMana;
+        }
+    }
+}
